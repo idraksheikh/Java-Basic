fe7fd4ffb24033e2c3fba67694f2f6626c0d5ea8
Idrak Sheikh <idraksheikh333@gmail.com>
2021-08-22 00:09:28 +0530

    9 th program

---
+++ Program9.java
@@ -0,0 +1,15 @@
+package classFile;
+
+public class Program9{
+	public static void main(String[] args) {
+		
+		int x=7;
+		// logic
+		int a=x*49/x+35/x;
+
+		System.out.println("The value of 'a' is : "+a);
+
+		
+
+	}
+}
