84104440f203d97ef86b00802c3717befba21660
Idrak Sheikh <idraksheikh333@gmail.com>
2021-08-21 23:24:30 +0530

    6 th Program

---
+++ Program6.java
@@ -0,0 +1,15 @@
+package classFile;
+
+public class Program6{
+	public static void main(String[] args) {
+		float a;	
+		//logic
+		a=7/4*9/2;
+		//take input form users
+		System.out.println("The output of following expression is : "+ a);
+		
+		
+		
+
+	}
+}
