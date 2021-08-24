b7514c3979e1b954ee5746cdc8e452c8d26ec46c
Idrak Sheikh <idraksheikh333@gmail.com>
2021-08-24 23:09:44 +0530

    22 th program

---
+++ looping_statement/Program22.java
@@ -0,0 +1,14 @@
+package classFile;
+
+public class Program22{
+	public static void main(String[] args) {
+		System.out.println("The multiplication table of 10 in reverse order is:-");
+		
+		for(int i=10; i>0;i--){
+			System.out.printf(" 10 X %d = %d",i,(10*i));
+			System.out.println("");
+
+		}	
+		
+	}
+}
