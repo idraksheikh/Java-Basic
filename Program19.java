d5515a2228c7c5de7c60a8d6d23d8fd5818994e5
Idrak Sheikh <idraksheikh333@gmail.com>
2021-08-24 22:11:33 +0530

    19 th Program

---
+++ Program19.java
@@ -0,0 +1,15 @@
+package classFile;
+
+public class Program19{
+	public static void main(String[] args) {
+		System.out.println("The pattern is as follows :-");
+		for(int i=4;i>0;i--){
+			for(int j=0;j<i;j++){
+				System.out.print(" * ");
+			}
+			System.out.println("");
+		}
+		
+	
+	}
+}
