e031aa0e5a5e6cd880a4b9f2bb088c479d7e9d71
Idrak Sheikh <idraksheikh333@gmail.com>
2021-08-26 22:44:25 +0530

    34th program

---
+++ methods/Program34.java
@@ -0,0 +1,17 @@
+package classFile;
+
+
+public class Program34{
+	public static void pattern1(){
+		for(int i=0;i<5;i++){
+			for(int j=0;j<i;j++){
+				System.out.print(" * ");
+			}
+			System.out.println("");
+		}
+	}
+	public static void main(String[] args) {
+		
+		pattern1();
+	}
+}
