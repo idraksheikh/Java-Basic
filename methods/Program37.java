d7a61c9ec35f2e2aa0101daf63b7c9edf48fb056
Idrak Sheikh <idraksheikh333@gmail.com>
2021-08-26 23:26:38 +0530

    37th program

---
+++ methods/Program37.java
@@ -0,0 +1,28 @@
+package classFile;
+import java.util.Scanner;
+
+public class Program37{
+	public  void fibo(int fnum,int snum,int num){
+		if(num==0){
+			int x=0;
+		}
+		else{
+			
+			int temp=snum;
+			snum=fnum+snum;
+			fnum=temp;
+			System.out.print(" "+snum);	
+			fibo(fnum,snum,num-1);
+			
+		}
+	}
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Please enter the number to which you want to print the Fibonacci series.");
+		int n=scan.nextInt();
+		Program37 obj=new Program37();
+		System.out.print(" 0 1");
+		obj.fibo(0,1,n-2);
+		
+	}
+}
